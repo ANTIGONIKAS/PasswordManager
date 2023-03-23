4a41916b50fc1d601c6163e40763d3bcc7ebafd5
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-03-23 18:28:38 +0200

    Install packages - Initial migration-update database

---
+++ PasswordManager.EF/Migrations/20230323160101_Initial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PasswordManager.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserEntries",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MasterUserName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MasterPassword = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEntries", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PasswordEntries",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Site = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    UserID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PasswordEntries", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PasswordEntries_UserEntries_UserID",
+                        column: x => x.UserID,
+                        principalTable: "UserEntries",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PasswordEntries_UserID",
+                table: "PasswordEntries",
+                column: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PasswordEntries");
+
+            migrationBuilder.DropTable(
+                name: "UserEntries");
+        }
+    }
+}
