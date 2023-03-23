d9542ad6edb73fd8eb9f94cca5b7e4946a7b7cfd
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-03-23 16:05:12 +0200

    Context-add class ApplicationContext

---
+++ PasswordManager.EF/Context/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordManager.EF.Configuration;
+using PasswordManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.EF.Context {
+    public class ApplicationContext:DbContext {
+        public DbSet<UserEntry> UserEntries { get; set; }
+        public DbSet<PasswordEntry> PasswordEntries { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserEntryConfiguration());
+            modelBuilder.ApplyConfiguration(new PasswordEntryConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PasswordManager;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
