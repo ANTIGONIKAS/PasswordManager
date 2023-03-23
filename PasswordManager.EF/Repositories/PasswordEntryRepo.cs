f5499edd6f82d54354097db07d4f44099d1308bc
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-03-23 20:01:53 +0200

    Repositories added

---
+++ PasswordManager.EF/Repositories/PasswordEntryRepo.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordManager.EF.Context;
+using PasswordManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.EF.Repositories {
+    public class PasswordEntryRepo : IEntityRepo<PasswordEntry> {
+        public void Add(PasswordEntry entity)
+        {
+            using var context = new ApplicationContext();
+            context.Add(entity);
+            context.SaveChanges();
+
+        }
+
+        public void Delete(Guid ID)
+        {
+            using var context = new ApplicationContext();
+            var PasswordEntryDb = context.PasswordEntries.Where(passwordEntry => passwordEntry.ID == ID).SingleOrDefault();
+            if (PasswordEntryDb is null)
+                return;
+            context.Remove(PasswordEntryDb);
+            context.SaveChanges();
+        }
+
+        public IList<PasswordEntry> GetAll()
+        {
+            using var context = new ApplicationContext();
+
+            return context.PasswordEntries.ToList();
+        }
+
+        public PasswordEntry? GetById(Guid ID)
+        {
+            using var context = new ApplicationContext();
+            return context.PasswordEntries
+                .Where(passwordEntry => passwordEntry.ID == ID)
+               
+                .SingleOrDefault();
+        }
+
+        public void Update(Guid ID, PasswordEntry entity)
+        {
+            using var context = new ApplicationContext();
+            var PasswordEntryDb = context.PasswordEntries
+                .Where(passwordEntry=> passwordEntry.ID == ID)
+                
+                .SingleOrDefault();
+            if (PasswordEntryDb is null) throw new KeyNotFoundException($"Given id '{ID}' was not found");
+            PasswordEntryDb.UserName = entity.UserName;
+            PasswordEntryDb.Password = entity.Password;
+            PasswordEntryDb.DateModified = entity.DateModified;
+            PasswordEntryDb.Site = entity.Site;
+            PasswordEntryDb.Active = entity.Active;
+            context.SaveChanges();
+        }
+    }
+}
---
+++ PasswordManager.EF/Repositories/UserEntryRepo.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordManager.EF.Context;
+using PasswordManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.EF.Repositories {
+    public class UserEntryRepo : IEntityRepo<UserEntry> {
+        public void Add(UserEntry entity)
+        {
+            using var context = new ApplicationContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+       
+
+        public IList<UserEntry> GetAll()
+        {
+            using var context = new ApplicationContext();
+            return context.UserEntries.Include(userEntry=>userEntry.StoredPasswords).ToList();
+
+        }
+
+        public UserEntry? GetById(Guid ID)
+        {
+            using var context = new ApplicationContext();
+            return context.UserEntries.Where(userEntry=>userEntry.ID==ID)
+            .Include(userEntry=>userEntry.StoredPasswords)
+            .SingleOrDefault();
+        }
+
+        public void Update(Guid ID, UserEntry entity)
+        {
+            using var context = new ApplicationContext();
+            var UserEntryDb = context.UserEntries
+               .Where(userEntry => userEntry.ID == ID)
+               .Include(userEntry => userEntry.StoredPasswords)
+               .SingleOrDefault();
+            if (UserEntryDb is null) throw new KeyNotFoundException($"Given id '{ID}' was not found");
+            UserEntryDb.MasterUserName = entity.MasterUserName;
+            UserEntryDb.MasterPassword = entity.MasterPassword;
+            UserEntryDb.DateCreated = entity.DateCreated;
+            UserEntryDb.FirstName = entity.FirstName;
+            UserEntryDb.LastName = entity.LastName;
+            UserEntryDb.Phone = entity.Phone;
+            UserEntryDb.Active = entity.Active;
+
+            context.SaveChanges();
+
+        }
+        public void Delete(Guid ID)
+        {
+            using var context = new ApplicationContext();
+            var UserEntryDb = context.UserEntries
+                .Where(userEntry=> userEntry.ID == ID)
+                .Include(userEntry => userEntry.StoredPasswords)
+                .SingleOrDefault();
+            if (UserEntryDb is null)
+                throw new KeyNotFoundException($"Given id '{ID}' was not found");
+            context.Remove(UserEntryDb);
+            context.SaveChanges();
+
+        }
+    }
+}
