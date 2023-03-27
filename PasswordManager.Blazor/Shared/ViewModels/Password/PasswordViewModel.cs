38d88c7dda9a9d6925ca16d71e9a5afed95d542a
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-03-27 19:31:48 +0300

    Added PasswordList

---
+++ PasswordManager.Blazor/Shared/ViewModels/Password/PasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Blazor.Shared.ViewModels.Password {
+    public class PasswordViewModel {
+        public Guid ID { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public string Site { get; set; }
+        public DateTime DateModified { get; set; }
+        public bool Active { get; set; }
+    }
+}
