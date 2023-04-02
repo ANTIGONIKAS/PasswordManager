d2b70529b73154a02d7bec35b04132b91dba2e19
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-04-02 19:35:07 +0300

    Done

--- PasswordManager.Blazor/Shared/Services/Authenticator.cs
+++ PasswordManager.Blazor/Shared/Services/Authenticator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PasswordManager.Blazor.Shared.ViewModels.Password;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace PasswordManager.Blazor.Shared.Services {
     public class Authenticator {
+        public UserEntryViewModel Authenticate ( List<UserEntryViewModel> lista, UserEntryViewModel login) {
 
-
+            foreach(var user in lista) {
+                if(user.MasterPassword== login.MasterPassword && user.MasterUserName==login.MasterUserName) {
+                    return user;
+                }
+            }
+            return null;
+        }
     }
 }
