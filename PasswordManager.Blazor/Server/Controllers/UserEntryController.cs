d4cfb7f14fe2e290029474c62524a799f49e277f
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-04-02 16:51:59 +0300

    changes+UserEntryController in progress

--- PasswordManager.Blazor/Server/Controllers/UserEntryController.cs
+++ PasswordManager.Blazor/Server/Controllers/UserEntryController.cs
@@ -1,10 +1,93 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PasswordManager.Blazor.Shared.ViewModels.Password;
+using PasswordManager.EF.Repositories;
+using PasswordManager.Model;
 
 namespace PasswordManager.Blazor.Server.Controllers {
     [Route("[controller]")]
     [ApiController]
     public class UserEntryController : ControllerBase {
 
+        private IEntityRepo<UserEntry> _userRepo;
+
+        public UserEntryController(IEntityRepo<UserEntry> userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        [HttpGet]
+
+        public async Task<IEnumerable<UserEntryViewModel>> Get()
+        {
+            var result = _userRepo.GetAll();
+            return result.Select(x => new UserEntryViewModel
+            {
+                ID = x.ID,
+                MasterUserName = x.MasterUserName,
+                MasterPassword = x.MasterPassword,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Phone = x.Phone,
+                DateCreated = x.DateCreated,
+                Active = x.Active,
+                passwords=x.StoredPasswords
+
+
+            });
+                
+        }
+        [HttpPost]
+
+        public async Task<ActionResult> Post(UserEntryViewModel user)
+        {
+            UserEntry newUserEntry = new();
+            newUserEntry.MasterUserName = user.MasterUserName;
+            newUserEntry.MasterPassword = user.MasterPassword;
+            newUserEntry.FirstName = user.FirstName;
+            newUserEntry.LastName = user.LastName;
+            newUserEntry.Phone = user.Phone;
+            newUserEntry.DateCreated = user.DateCreated;
+            newUserEntry.Active = true;
+            _userRepo.Add(newUserEntry);
+            return Ok();
+
+
+
+        }
+        [HttpPut]
+
+        public async Task<ActionResult> Put(UserEntryViewModel newUser)
+        {
+            var foundUser = _userRepo.GetById(newUser.ID);
+
+            if (foundUser != null)
+            {
+                foundUser.MasterUserName = newUser.MasterUserName;
+                foundUser.MasterPassword = newUser.MasterPassword;
+                foundUser.FirstName = newUser.FirstName;
+                foundUser.LastName = newUser.LastName;
+                foundUser.Phone = newUser.Phone;
+                foundUser.DateCreated = newUser.DateCreated;
+                foundUser.Active = newUser.Active;
+                _userRepo.Update(newUser.ID, foundUser);
+                return Ok();
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+                
+        }
+        [HttpGet("{id}")]
+
+        public async Task<UserEntryViewModel> Get(Guid id)
+        {
+            var result = _userRepo.GetById(id);
+
+            UserEntryViewModel tmp = new();
+            tmp.ID = result.ID;
+           
+        }
     }
 }
