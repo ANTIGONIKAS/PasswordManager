6478ea6d780a01c7a49f5e478fc1374ccd6d1212
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-09-03 19:14:14 +0300

    Validation Examples for later lesson

--- PasswordManager.Blazor/Shared/ViewModels/UserViewModel.cs
+++ PasswordManager.Blazor/Shared/ViewModels/UserViewModel.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//TODO: https://learn.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-aspnet-mvc3/cs/adding-validation-to-the-model
 
 
 namespace PasswordManager.Blazor.Shared.User {
@@ -12,14 +13,17 @@ namespace PasswordManager.Blazor.Shared.User {
 
 
         [Required]
-        [MaxLength(20, ErrorMessage = " Max length 20 characters.")]
+        [MaxLength(20, ErrorMessage = " Max length 20 characters.")] 
         public string? FName { get; set; }
 
         [Required]
         [MaxLength(20, ErrorMessage = " Max length 20 characters.")]
         public string? LName { get; set; }
 
+        [Required]
+        [MaxLength(50,ErrorMessage = "Master Username max length is 50 characters")]
         public string? MasterUsername { get; set; }
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,16}$", ErrorMessage = "Password Must Have a lower,upper,number and a special character.")]
         public string? MasterPassword { get; set; }
         public string? Email { get; set; }
 
